217d936e37b410457bf6481045e4974594276da5
zeesec <zeesecacc2@gmail.com>
2023-03-11 10:56:54 +0700

    kotno

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        clrfigma: '#7F56DA'
+      }
+    },
+  },
+  plugins: [],
+}
